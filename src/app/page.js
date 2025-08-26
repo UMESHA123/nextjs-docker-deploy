8550260d30b0ba64b23444bc883f639b5ab30636
UMESHA123 <uumesharameshahugger@gmail.com>
2025-08-27 00:42:04 +0530

    initial code commit: nextjs application

---
+++ src/app/page.js
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+   <div>
+    <h1 style={{display:'flex', alignItems: 'center', justifyContent:'center', height:'100vh'}}>Next js is up and running</h1>
+   </div>
+  );
+}
